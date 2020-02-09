bf0e122131455ef122cd967671547948555532b9
unknown <danncem@gmail.com>
2020-02-09 19:09:13 +0200

    vectors+velocity

--- task_2.py
+++ task_2.py
@@ -1,6 +1,8 @@
 from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
 import numpy as np
+import math  
+
 #drawing a plot based on specific function for each coordinate
 
 fig = plt.figure()
@@ -12,16 +14,32 @@ increment = np.arange(m)
 x = np.empty(m)
 y = np.empty(m)
 z = np.empty(m)
+u = np.empty(m)
+v = np.empty(m)
+w = np.empty(m)
+# for i in range (m):
+#     x[i] = input("x_{i} coord")
+#     y[i] = input("y_{i} coord")
+#     z[i] = input("z_{i} coord")
 
-for i in range (m-1):
-    x[i] = 0
-    y[i] = 0
-    z[i] = 0 
+x = [1, 10,10,1]
+y = [1, 1, 10,10]
+z = [10,10,1, 1]
 
+for i in range(m-1):
+    u[i]=-x[i]+x[i+1]
+    v[i]=-y[i]+y[i+1]
+    w[i]=-z[i]+z[i+1]
 
+u[m-1]=-x[m-1]+x[0]
+v[m-1]=-y[m-1]+y[0]
+w[m-1]=-z[m-1]+z[0]
 
-ax.plot3D(x, y, z, 'gray')
-plt.title('blabla')
-plt.xlabel('x')
-plt.ylabel('y')
+ax.quiver(x, y, z, u, v, w, arrow_length_ratio =0.1)
+
+#ax.plot3D(x, y, z, 'gray')
+plt.title('Vector Square')
+plt.xlabel('x', color = "blue")
+plt.ylabel('y', color = "blue")
+ax.set_zlabel('z', color = "blue")
 plt.show()
---
+++ task_3.py
@@ -0,0 +1,77 @@
+from mpl_toolkits.mplot3d import axes3d
+import matplotlib.pyplot as plt
+import numpy as np
+
+#main task trial
+#pre configs
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+
+#number of points
+m = 2 
+
+#G
+GAMMA=1.0
+#dots A,B
+x = np.empty(m)
+y = np.empty(m)
+z = np.empty(m)
+
+dr_x = np.empty(m)
+dr_y = np.empty(m)
+dr_z = np.empty(m)
+
+#dot V
+grid_x =np.empty(1)
+grid_y =np.empty(1)
+grid_z =np.empty(1)
+
+r_x = np.empty(m)
+r_y = np.empty(m)
+r_z = np.empty(m)
+
+x = [3,3]
+y = [1,5]
+z = [0,4]
+grid_x = [7]
+grid_y = [2]
+grid_z = [1]
+
+#setting vector coords for AB
+for i in range(m-1):
+    dr_x[i]=x[i+1]-x[i]
+    dr_y[i]=y[i+1]-y[i]
+    dr_z[i]=z[i+1]-z[i]
+
+#setting vector coords for AV:
+r_x = grid_x[0] -x[0]
+r_y = grid_y[0] -y[0]
+r_z = grid_z[0] -z[0]
+
+#finding length of AV:
+r_length = (r_x**2 + r_y**2 + r_z**2)**(1/2)
+r_squared = r_length**3
+
+du = (dr_y[0]*r_z - dr_z[0]*r_y)/r_squared
+dv = (dr_z[0]*r_x-dr_x[0]*r_z)/r_squared
+dw = (dr_x[0]*r_y-dr_y[0]*r_x)/r_squared
+
+velocity_x = (GAMMA/4*np.pi)*du
+velocity_y = (GAMMA/4*np.pi)*dv
+velocity_z = (GAMMA/4*np.pi)*dw
+
+plt.plot(grid_x, grid_y,grid_z, 'ro')
+plt.plot(x, y, z, 'ro')
+
+ax.quiver(x[0], y[0], z[0], dr_x, dr_y, dr_z, length =1,arrow_length_ratio =0.1)
+ax.quiver(x[0], y[0], z[0], r_x, r_y, r_z, length =1, arrow_length_ratio =0.1)
+
+ax.quiver(grid_x[0], grid_y[0],grid_z[0],
+velocity_x-grid_x[0],velocity_y-grid_y[0],velocity_z-grid_z[0],
+length = 0.5, arrow_length_ratio =0.1, color = "red")
+
+plt.title('Task 4')
+plt.xlabel('x', color = "blue")
+plt.ylabel('y', color = "blue")
+ax.set_zlabel('z', color = "blue")
+plt.show()
