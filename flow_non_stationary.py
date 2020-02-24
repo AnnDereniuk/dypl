ef7d2ca78960ea577ed8abe84f979511d30710c3
unknown <danncem@gmail.com>
2020-02-24 09:57:54 +0200

    velocity 1st version

--- flow_non_stationary.py
+++ flow_non_stationary.py
@@ -2,7 +2,8 @@ from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
 import numpy as np
 from some_functions import *
-
+GAMMA_ZERO = 0.
+VELOCITY_ON_INF = 0.7
 #main task trial
 #pre configs
 np.set_printoptions(precision=13)
@@ -10,8 +11,8 @@ fig = plt.figure(figsize=(6,6))
 ax = plt.axes()
 
 #grid
-g = 15
-dots_quantity = 30
+dots_quantity = 50
+g = dots_quantity/2
 grid_vertical = np.linspace(-g, g+2, dots_quantity)
 grid_horizontal = np.linspace(-g, g-2, dots_quantity)
 grid_x,grid_y = np.meshgrid(grid_vertical, grid_horizontal)
@@ -21,7 +22,6 @@ print(grid_x)
 print(grid_y)
 
 m = 10   #half-number of vortex dots
-velocity_on_inf = 1
 
 x_center = 3
 y_center = 6
@@ -78,12 +78,12 @@ for j in range(0, k):
 
             left_matrix[j][i] = left_matrix_part_x[j][i]+left_matrix_part_y[j][i]
 
-            right_matrix_part_x[j] = -velocity_on_inf*normal_x[i]
-            right_matrix_part_y[j] = -velocity_on_inf*normal_y[i]
+            right_matrix_part_x[j] = -VELOCITY_ON_INF*normal_x[i]
+            right_matrix_part_y[j] = -VELOCITY_ON_INF*normal_y[i]
             
             right_matrix[j] = right_matrix_part_x[j]+right_matrix_part_y[j]
             left_matrix[k-1][i] = 1.
-right_matrix[k-1] = 1.
+right_matrix[k-1] = GAMMA_ZERO
 # print("Ліва частина СЛАР:")
 # print(left_matrix)
 # print("Права частина СЛАР:")
@@ -95,22 +95,48 @@ gamma_arr=np.linalg.solve(left_matrix, right_matrix)
 print("Гамма:")
 print(gamma_arr)
 
-velocity_x=np.empty(dots_quantity**2)
-velocity_y=np.empty(dots_quantity**2)
-for velocity in range(0,dots_quantity**2-1):
-    for i,j in range(0,dots_quantity):
+# velocity_x=np.empty(dots_quantity)
+# velocity_y=np.empty(dots_quantity)
+# for i in range(0,dots_quantity):
+#     for j in range(0,dots_quantity):
+#         for v in range(0,k):
+#             velocity_x[i]+=gamma_arr[v]*get_velocity_j(x[v], grid_x[i][j], get_R(grid_x[i][j], grid_y[i][j], x[v], x[v+1], y[v], y[v+1]))
+#             velocity_y[i]+=gamma_arr[v]*get_velocity_j(grid_y[i][j], y[v], get_R(grid_y[i][j], grid_y[i][j],x[v], x[v+1], y[v], y[v+1]))
+#             velocity_x[i]+=VELOCITY_ON_INF
+#             velocity_y[i]+=VELOCITY_ON_INF 
+
+
+velocity_x=np.array([])
+velocity_y=np.array([])
+grid_x=np.asarray(grid_x).reshape(-1)
+grid_y=np.asarray(grid_y).reshape(-1)
+print(grid_x)
+
+# for i in range(0,dots_quantity**2):
+#     for v in range(0,k):
+#         velocity_x[i]+=gamma_arr[v]*get_velocity_j(x[v], grid_x[i], get_R(grid_x[i], grid_y[i], x[v], x[v+1], y[v], y[v+1]))
+#         velocity_y[i]+=gamma_arr[v]*get_velocity_j(grid_y[i], y[v], get_R(grid_y[i], grid_y[i],x[v], x[v+1], y[v], y[v+1]))
+#     velocity_x[i]+=VELOCITY_ON_INF
+#     velocity_y[i]+=VELOCITY_ON_INF 
+
+for i in range(0,dots_quantity):
+    for j in range(0,dots_quantity):
+        res_x=0.
+        res_y=0.
         for v in range(0,k):
-            velocity_x[velocity]+=gamma_arr[v]*get_velocity_j(x[v], grid_x[i][j], get_R(grid_x[i][j], grid_y[i][j], x[v], x[v+1], y[v], y[v+1]))
-            velocity_y[velocity]+=gamma_arr[v]*get_velocity_j(grid_y[i][j], y[v], get_R(grid_y[i][j], grid_y[i][j],x[v], x[v+1], y[v], y[v+1]))
-            velocity_x[velocity]+=velocity_on_inf
-            velocity_y[velocity]+=velocity_on_inf 
+            res_x+=gamma_arr[v]*get_velocity_j(x[v], grid_horizontal[i], get_R(grid_horizontal[i], grid_vertical[j], x[v], x[v+1], y[v], y[v+1]))
+            res_y+=gamma_arr[v]*get_velocity_j(grid_vertical[j], y[v], get_R(grid_horizontal[i], grid_vertical[j], x[v], x[v+1], y[v], y[v+1]))
+        res_x+=VELOCITY_ON_INF
+        res_y+=VELOCITY_ON_INF
+        velocity_x=np.append(velocity_x, res_x)
+        velocity_y=np.append(velocity_y, res_y)
 
 plt.plot(x, y, 'ro', markersize=2)                                      #vortices
 plt.plot(x, y, 'gray')
 plt.plot(colocation_x, colocation_y, 'bo', markersize=2)                #colocation dots
 # ax.quiver(colocation_x, colocation_y, normal_x, normal_y)               #normals
 plt.plot(grid_x,grid_y, 'ko', markersize=1)                             #grid dots
-ax.quiver(grid_x,grid_y, velocity_x,velocity_y)                         #velocity
+ax.quiver(grid_x,grid_y, velocity_x,velocity_y, norm=TRUE)                         #velocity
 
 plt.title('plot')
 plt.xlabel('x')
