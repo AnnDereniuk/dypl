f1795176caefe2fc5cf8492637dac68cc94cbd2f
unknown <danncem@gmail.com>
2020-02-25 13:17:12 +0200

    minor changes

--- flow_non_stationary.py
+++ flow_non_stationary.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from some_functions import *
 GAMMA_ZERO = 0.
-VELOCITY_ON_INF = 0.7
+VELOCITY_ON_INF = 5.
 #main task trial
 #pre configs
 np.set_printoptions(precision=13)
@@ -11,15 +11,15 @@ fig = plt.figure(figsize=(6,6))
 ax = plt.axes()
 
 #grid
-dots_quantity = 50
+dots_quantity = 70
 g = dots_quantity/2
 grid_vertical = np.linspace(-g, g+2, dots_quantity)
 grid_horizontal = np.linspace(-g, g-2, dots_quantity)
 grid_x,grid_y = np.meshgrid(grid_vertical, grid_horizontal)
 
-print("grif х,у:")
-print(grid_x)
-print(grid_y)
+# print("grid х,у:")
+# print(grid_x)
+# print(grid_y)
 
 m = 10   #half-number of vortex dots
 
@@ -53,18 +53,18 @@ for i in range(0,k):
     colocation_x[i] = (x[i] + x[i+1])/2.
     colocation_y[i] = (y[i] + y[i+1])/2.
 
-print("х,у колокацій:")
-print(colocation_x)
-print(colocation_y)
+# print("х,у колокацій:")
+# print(colocation_x)
+# print(colocation_y)
 
 normal_x=np.empty(k)
 normal_y=np.empty(k)
 for i in range(0,k):
     normal_y[i]=(x[i+1]-x[i])/get_vector_length_2d(get_vector_coords(x[i],x[i+1]), get_vector_coords(y[i],y[i+1]))
     normal_x[i]=-(y[i+1]-y[i])/get_vector_length_2d(get_vector_coords(x[i],x[i+1]), get_vector_coords(y[i],y[i+1]))
-print("х, у нормалей:")
-print(normal_x)
-print(normal_y)
+# print("х, у нормалей:")
+# print(normal_x)
+# print(normal_y)
 
 left_matrix_part_x = left_matrix_part_y = left_matrix= np.empty(shape=(k,k))
 right_matrix_part_x = right_matrix_part_y = right_matrix = np.empty(k)
@@ -76,12 +76,12 @@ for j in range(0, k):
 
             left_matrix_part_y[j][i] = get_velocity_j(colocation_y[i], y[j], get_R(colocation_x[i], colocation_y[i],x[j], x[j+1], y[j],y[j+1]))*normal_y[i]
 
-            left_matrix[j][i] = left_matrix_part_x[j][i]+left_matrix_part_y[j][i]
+            left_matrix[j][i] = left_matrix_part_x[j][i] + left_matrix_part_y[j][i]
 
             right_matrix_part_x[j] = -VELOCITY_ON_INF*normal_x[i]
             right_matrix_part_y[j] = -VELOCITY_ON_INF*normal_y[i]
             
-            right_matrix[j] = right_matrix_part_x[j]+right_matrix_part_y[j]
+            right_matrix[j] = right_matrix_part_x[j] + right_matrix_part_y[j]
             left_matrix[k-1][i] = 1.
 right_matrix[k-1] = GAMMA_ZERO
 # print("Ліва частина СЛАР:")
@@ -89,7 +89,7 @@ right_matrix[k-1] = GAMMA_ZERO
 # print("Права частина СЛАР:")
 # print(right_matrix)
 
-gamma_arr = np.empty(2*m)
+gamma_arr = np.empty(k)
 gamma_arr=np.linalg.solve(left_matrix, right_matrix)
 
 print("Гамма:")
@@ -131,16 +131,13 @@ for i in range(0,dots_quantity):
         velocity_x=np.append(velocity_x, res_x)
         velocity_y=np.append(velocity_y, res_y)
 
-plt.plot(x, y, 'ro', markersize=2)                                      #vortices
-plt.plot(x, y, 'gray')
-plt.plot(colocation_x, colocation_y, 'bo', markersize=2)                #colocation dots
+plt.plot(y, x, 'ro', markersize=2)                                      #vortices
+plt.plot(y, x, 'gray')
+# plt.plot(colocation_x, colocation_y, 'bo', markersize=2)                #colocation dots
 # ax.quiver(colocation_x, colocation_y, normal_x, normal_y)               #normals
-plt.plot(grid_x,grid_y, 'ko', markersize=1)                             #grid dots
-ax.quiver(grid_x,grid_y, velocity_x,velocity_y, norm=TRUE)                         #velocity
-
+# plt.plot(grid_x,grid_y, 'ko', markersize=1)                             #grid dots
+ax.quiver(grid_x,grid_y, velocity_x,velocity_y,linewidth=0.1, norm=TRUE)          #velocity
 plt.title('plot')
 plt.xlabel('x')
 plt.ylabel('y')
 plt.show()
-
-
